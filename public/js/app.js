dc2c6af19f4313d6d9b85eed5856e82a679fae0e
Ahmed Samir <ahmedhtml@yahoo.com>
2021-07-06 13:56:44 +0200

    Eddited soem pages

--- public/js/app.js
+++ public/js/app.js
@@ -6,9 +6,12 @@ window.onload = () =>{
     const search = document.querySelector('input');
     const m1 = document.getElementById('p1');
     const m2 = document.getElementById('p2')
+    const con = document.getElementById('con');
 
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
+        con.style.visibility = "visible";
+
         const location = search.value
         p2.textContent = "Loading data..."
 
